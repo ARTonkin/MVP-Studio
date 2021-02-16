124f56a885d7da116a56e4691257763e71642c15
ARTonkin <43068403+ARTonkin@users.noreply.github.com>
2021-02-16 16:00:06 +1300

    Update MVP_onboarding.sql

--- MVP_onboarding.sql
+++ MVP_onboarding.sql
@@ -44,13 +44,22 @@ SELECT
     p.Name,
 	TotalPayment = 
 	CASE 
-		WHEN tp.PaymentFrequencyId = 1 THEN 
-			DATEDIFF(WEEK, tp.StartDate, tp.EndDate) * tp.PaymentAmount
-		WHEN tp.PaymentFrequencyId = 2 THEN 
-			DATEDIFF(WEEK, tp.StartDate, tp.EndDate) / 2 * tp.PaymentAmount
-		WHEN tp.PaymentFrequencyId = 3 THEN 
-			DATEDIFF(MONTH, tp.StartDate, tp.EndDate) * tp.PaymentAmount
-	END, pf.Yield
+		WHEN trt.Name = 'Weekly' THEN 
+			DATEDIFF(WEEK, tp.StartDate, tp.EndDate) * prp.Amount
+		WHEN trt.Name = 'Fortnightly' THEN 
+			DATEDIFF(WEEK, tp.StartDate, tp.EndDate) / 2 * prp.Amount
+		WHEN trt.Name = 'Monthly' THEN
+			DATEDIFF(MONTH, tp.StartDate, tp.EndDate) * prp.Amount
+	END, 
+	Yield = 
+	(CASE 
+		WHEN trt.Name = 'Weekly' THEN 
+			DATEDIFF(WEEK, tp.StartDate, tp.EndDate) * prp.Amount
+		WHEN trt.Name = 'Fortnightly' THEN 
+			DATEDIFF(WEEK, tp.StartDate, tp.EndDate) / 2 * prp.Amount
+		WHEN trt.Name = 'Monthly' THEN
+			DATEDIFF(MONTH, tp.StartDate, tp.EndDate) * prp.Amount
+	END) / phv.Value * 100
 FROM
     Property p
         LEFT JOIN
@@ -58,10 +67,12 @@ FROM
 		LEFT JOIN
 	TenantProperty tp ON p.Id = tp.PropertyId
 		LEFT JOIN
-	TenantPaymentFrequencies tpf ON tp.PaymentFrequencyId = tpf.Id
+	PropertyHomeValue phv ON p.Id = phv.PropertyId
 		LEFT JOIN
-	PropertyFinance pf ON p.Id = pf.PropertyId
-WHERE op.OwnerId = 1426
+	PropertyRentalPayment prp ON p.Id = prp.PropertyId
+		LEFT JOIN
+	TargetRentType trt ON prp.FrequencyType = trt.Id 
+WHERE op.OwnerId = 1426 AND phv.IsActive = 1
 ORDER BY p.Name;
 
 
@@ -71,15 +82,13 @@ Display all the jobs available.
 */
 
 SELECT DISTINCT 
-	j.JobDescription
+	j.Id, j.PropertyId, j.JobDescription
 FROM 
 	Job j
 		LEFT JOIN
-	JobStatus js ON j.JobStatusId = js.Id
-		LEFT JOIN
 	JobMedia jm ON j.Id = jm.JobId
-WHERE js.Status = 'Open' AND jm.IsActive = 1
-ORDER BY j.JobDescription;
+WHERE jm.IsActive = 1
+ORDER BY j.Id;
 
 
 /*
@@ -90,7 +99,7 @@ Display all property names, current tenants first and last names
 */
 
 SELECT
-    p.Name, pe.FirstName, pe.LastName, tp.PaymentAmount, tpf.Code
+    p.Name, pe.FirstName, pe.LastName, prp.Amount, tpf.Code
 FROM
     Property p
         LEFT JOIN
@@ -100,10 +109,10 @@ FROM
 		LEFT JOIN
 	Tenant t ON tp.TenantId = t.Id
 		LEFT JOIN
-	Person pe ON t.Id = pe.Id
+	Person pe ON tp.TenantId = pe.Id
 		LEFT JOIN
 	TenantPaymentFrequencies tpf ON tp.PaymentFrequencyId = tpf.Id
+		LEFT JOIN
+	PropertyRentalPayment prp ON p.Id = prp.PropertyId
 WHERE op.OwnerId = 1426 AND t.IsActive = 1
 ORDER BY p.Name;
-
-
